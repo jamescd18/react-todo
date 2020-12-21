9033a01004636c811e031d0bc34c13805fe7caed
James C-D <james.changdavidson@gmail.com>
2020-12-21 02:18:52 -0800

    add done handler

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -2,17 +2,31 @@ import React from 'react';
 
 const ToDo = ({ toDo, toDos, setToDos }) => {
 
+    const doneHandler = (e) => {
+        setToDos(toDos.map((item) => {
+            if (item.id === toDo.id) {
+                return {
+                    ...item,
+                    done: !item.done
+                }
+            }
+            return item;
+        }));
+    };
+
     const deleteHandler = (e) => {
         setToDos(toDos.filter(ele => ele.id !== toDo.id));
     };
 
     return (
         <div className="todo">
-            <li className="todo-item">{ toDo.text }</li>
-            <button className="complete-btn">
+            <li className="todo-item">
+                { toDo.text }
+                </li>
+            <button onClick={doneHandler} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
-            <button onClick={ deleteHandler } className="trash-btn">
+            <button onClick={deleteHandler} className="trash-btn">
                 <i className="fas fa-trash"></i>
             </button>
         </div>
